20c7da78568f19357cff90a67f756f9a75897e42
Алексей Иванов <alekseqolegovi26ivanov@bk.ru>
2024-01-11 22:23:03 +0300

    конец!!!

--- app.py
+++ app.py
@@ -1,16 +1,96 @@
-# This is a sample Python script.
+import sys
+import buttons
+import password
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from ui_main import Ui_MainWindow
 
+from PySide6.QtWidgets import QApplication, QMainWindow, QLineEdit
+from PySide6.QtGui import QCloseEvent
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class PasswordGenerator(QMainWindow):
+    def __init__(self):
+        super(PasswordGenerator, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.connect_slider_to_spinbox()
+        self.set_password()
+        self.do_when_password_edit()
+        for btn in buttons.GENERATE_PASSWORD:
+            getattr(self.ui, btn).clicked.connect(self.set_password)
+        self.ui.btn_visibility.clicked.connect(self.change_password_visibility)
+        self.ui.btn_copy.clicked.connect(self.copy_to_clipboard)
 
+    def connect_slider_to_spinbox(self) -> None:
+        self.ui.slider_length.valueChanged.connect(self.ui.spinbox_length.setValue)
+        self.ui.spinbox_length.valueChanged.connect(self.ui.slider_length.setValue)
+        self.ui.spinbox_length.valueChanged.connect(self.set_password)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def get_characters(self) -> str:
+        chars = ""
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        for btn in buttons.Characters:
+            if getattr(self.ui, btn.name).isChecked():
+                chars += btn.value
+
+        return chars
+
+    def set_password(self) -> None:
+        try:
+            self.ui.line_password.setText(
+                password.create_new(
+                    length=self.ui.slider_length.value(),
+                    characters=self.get_characters())
+            )
+        except IndexError:
+            self.ui.line_password.clear()
+        self.set_entropy()
+        self.set_strength()
+
+    def get_character_number(self) -> int:
+        num = 0
+
+        for btn in buttons.CHARACTER_NUMBER.items():
+            if getattr(self.ui, btn[0]).isChecked():
+                num += btn[1]
+
+        return num
+
+    def set_entropy(self) -> None:
+        length = len(self.ui.line_password.text())
+        char_num = self.get_character_number()
+
+        self.ui.label_entropy.setText(
+            f"Entropy: {password.get_entropy(length, char_num)} bit"
+        )
+
+    def set_strength(self) -> None:
+        length = len(self.ui.line_password.text())
+        char_num = self.get_character_number()
+
+        for strength in password.StrengthToEntropy:
+            if password.get_entropy(length, char_num) >= strength.value:
+                self.ui.label_strength.setText(f"Strength: {strength.name}")
+
+    def change_password_visibility(self) -> None:
+        if self.ui.btn_visibility.isChecked():
+            self.ui.line_password.setEchoMode(QLineEdit.Normal)
+        else:
+            self.ui.line_password.setEchoMode(QLineEdit.Password)
+
+    def copy_to_clipboard(self) -> None:
+        QApplication.clipboard().setText(self.ui.line_password.text())
+
+    def closeEvent(self, event: QCloseEvent) -> None:
+        QApplication.clipboard().clear()
+
+    def do_when_password_edit(self) -> None:
+        self.ui.line_password.textEdited.connect(self.set_entropy)
+        self.ui.line_password.textEdited.connect(self.set_strength)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = PasswordGenerator()
+    window.show()
+
+    sys.exit(app.exec())
